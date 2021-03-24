ce61ec4145893b5783c1f8f2c4681cdabf7b4e64
Tom <tombenyunes@gmail.com>
2021-03-24 18:30:03 +0000

    update foods

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,9 @@ module.exports = function (app)
 {
     const utils = require('../lib/utils.js');
 
-    // admin database management, clear database
-	app.get('/deleteall', utils.redirectAdmin, (req,res) => 
+    // admin database management, clear collections
+
+	app.get('/clearusers', utils.redirectAdmin, (req,res) => 
 	{
 		var MongoClient = require('mongodb').MongoClient;
 		var url = process.env.DATABASE_PATH;
@@ -13,10 +14,26 @@ module.exports = function (app)
 			if (err) throw err;
 			var db = client.db(process.env.DATABASE_NAME);
 			db.collection('users').deleteMany({}); // delete all from 'users'
+			
+			let title = 'Success';
+			let message = 'All users have been cleared.';
+			res.render('templates/messageTemplate.html', {title: title, message: message, multipleMessages: false, color: '#6a9955'}); // success message
+		});
+	});
+
+	app.get('/clearfoods', utils.redirectAdmin, (req,res) => 
+	{
+		var MongoClient = require('mongodb').MongoClient;
+		var url = process.env.DATABASE_PATH;
+
+		MongoClient.connect(url, function (err, client)
+		{
+			if (err) throw err;
+			var db = client.db(process.env.DATABASE_NAME);
 			db.collection('foods').deleteMany({}); // delete all from 'foods'
 			
 			let title = 'Success';
-			let message = 'All data has been cleared.';
+			let message = 'All foods have been cleared.';
 			res.render('templates/messageTemplate.html', {title: title, message: message, multipleMessages: false, color: '#6a9955'}); // success message
 		});
 	});
--- routes/food.js
+++ routes/food.js
@@ -19,16 +19,82 @@ module.exports = function (app)
             var db = client.db(process.env.DATABASE_NAME);
             db.collection(process.env.COLLECTION_FOODS).insertOne({ // insert food name and price into database
                 name: req.body.name,
-                price: req.body.price
+                price: req.body.price,
+                typicalValues: req.body.typicalValues,
+                typicalValuesUnit: req.body.typicalValuesUnit,
+                calories: req.body.calories,
+                carbohydrates: req.body.carbohydrates,
+                fat: req.body.fat,
+                protein: req.body.protein,
+                salt: req.body.salt,
+                sugar: req.body.sugar,
+                author: req.body.author
             });
             client.close();
-            let title = 'Food Added';
-            let message = 'Food has been added to the database, name: "' + req.body.name + '", price £' + req.body.price + '.'; // success message
+            let title = 'Food Added';            
+            let message = '"' + req.body.name + '" has been added to the database.'; // success message
+            // let message = 'Food added: ' + req.body.name + " " + req.body.price + " " + req.body.typicalValues + " " + req.body.typicalValuesUnit + " " + req.body.calories + " " + req.body.carbohydrates + " " + req.body.fat + " " + req.body.protein + " " + req.body.salt + " " + req.body.sugar + " " + req.body.author;
             res.render('templates/messageTemplate.html', { title: title, message: message, multipleMessages: false, color: '#6a9955' });
         });
     });
 
-	// list all foods in collection
+    app.get('/updatefood', utils.redirectLogin, function (req, res) {
+        res.render("updatefood.html");
+    });
+
+    app.get('/update-result', function (req, res) {
+        var MongoClient = require('mongodb').MongoClient;
+        var url = process.env.DATABASE_PATH;
+
+        MongoClient.connect(url, function (err, client) {
+            if (err) throw err;
+            var db = client.db(process.env.DATABASE_NAME);
+            db.collection(process.env.COLLECTION_FOODS).find({ name: { $regex: req.query.keyword, $options: 'i'  } }).toArray(function (err, result) // find all foods that contain characters from the search query (non-case sensitive)
+            {
+                if (err) {
+                    res.redirect('./search'); // redirect upon error
+                } else {
+                    res.render('update_result.html', { availableFoods: result, username: req.session.userId }); // display all foods that match query
+                }
+                client.close();
+            });
+        });
+    });
+
+    app.post('/foodupdated', function (req, res) {
+        var MongoClient = require('mongodb').MongoClient;
+        var url = process.env.DATABASE_PATH;
+
+        MongoClient.connect(url, function (err, client) {
+            if (err) throw err;
+            var db = client.db(process.env.DATABASE_NAME);
+
+            db.collection(process.env.COLLECTION_FOODS).findOneAndUpdate({
+                name: req.body.name
+            },
+            {
+                $set: 
+                {
+                    name: req.body.name,
+                    price: req.body.price,
+                    typicalValues: req.body.typicalValues,
+                    typicalValuesUnit: req.body.typicalValuesUnit,
+                    calories: req.body.calories,
+                    carbohydrates: req.body.carbohydrates,
+                    fat: req.body.fat,
+                    protein: req.body.protein,
+                    salt: req.body.salt,
+                    sugar: req.body.sugar
+                }
+            });
+            client.close();
+            let title = 'Food Updated';
+            let message = '"' + req.body.name + '" has been updated.'; // success message
+            // let message = 'Food added: ' + req.body.name + " " + req.body.price + " " + req.body.typicalValues + " " + req.body.typicalValuesUnit + " " + req.body.calories + " " + req.body.carbohydrates + " " + req.body.fat + " " + req.body.protein + " " + req.body.salt + " " + req.body.sugar + " " + req.body.author;
+            res.render('templates/messageTemplate.html', { title: title, message: message, multipleMessages: false, color: '#6a9955' });
+        });
+    });
+
 	app.get('/listfoods', utils.redirectLogin, function (req, res)
 	{
 		var MongoClient = require('mongodb').MongoClient;
@@ -59,7 +125,7 @@ module.exports = function (app)
         MongoClient.connect(url, function (err, client) {
             if (err) throw err;
             var db = client.db(process.env.DATABASE_NAME);
-            db.collection(process.env.COLLECTION_FOODS).find({ name: { $regex: req.query.keyword } }).toArray(function (err, result) // find all foods that contain characters from the search query
+            db.collection(process.env.COLLECTION_FOODS).find({ name: { $regex: req.query.keyword, $options: 'i' } }).toArray(function (err, result) // find all foods that contain characters from the search query (non-case sensitive)
             {
                 if (err) {
                     res.redirect('./search'); // redirect upon error
